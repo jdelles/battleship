174b0803bc3a0302926987e1d910b072b713b4be
jdelles <james.delles@gmail.com>
2022-05-13 16:31:00 -0400

    Bug in gameboard placeShip

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,64 +1,73 @@
 const gameBoard = require("../src/gameboard");
 
 test("gameboard - construction", () => {
-    const testBoard = new gameBoard();
-
-    expect(testBoard.board.length).toBe(10);
-    expect(testBoard.board[0].length).toBe(10);
+    console.log("gameboard - construction - enter");
+    const testBoard = gameBoard();
 
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
-            expect(testBoard.board[i][j]).toBe(0);
+            expect(testBoard.getPosition(i, j)).toBe(0);
         }
     }
+    console.log("gameboard - construction - exit");
 });
 
 test("gameboard - place ships", () => {
-    const testBoard = new gameBoard();
+    console.log("gameboard - place ships - enter");
+    const testBoard2 = gameBoard();
 
-    testBoard.placeShip(0, 0, 5, "horizontal");
-    expect(testBoard.shipCount).toBe(1);
+    const shipCounter = testBoard2.counterCreator();
+
+    console.log("gameboard - place ships - ship 1");
+    const name1 = `Ship-${shipCounter.incCounter()}`;
     const ship1 = `Ship-1`;
+    testBoard2.placeShip(name1, 0, 0, 5, "horizontal");
 
-    expect(testBoard.board[0][0].ship.name).toBe(ship1);
-    expect(testBoard.board[0][0].position).toBe(0);
-    expect(testBoard.board[0][1].ship.name).toBe(ship1);
-    expect(testBoard.board[0][1].position).toBe(1);
-    expect(testBoard.board[0][2].ship.name).toBe(ship1);
-    expect(testBoard.board[0][2].position).toBe(2);
-    expect(testBoard.board[0][3].ship.name).toBe(ship1);
-    expect(testBoard.board[0][3].position).toBe(3);
-    expect(testBoard.board[0][4].ship.name).toBe(ship1);
-    expect(testBoard.board[0][4].position).toBe(4);
+    expect(testBoard2.getPosition(0, 0).ship.name).toBe(ship1);
+    expect(testBoard2.getPosition(0, 0).position).toBe(0);
+    expect(testBoard2.getPosition(0, 1).ship.name).toBe(ship1);
+    expect(testBoard2.getPosition(0, 1).position).toBe(1);
+    expect(testBoard2.getPosition(0, 2).ship.name).toBe(ship1);
+    expect(testBoard2.getPosition(0, 2).position).toBe(2);
+    expect(testBoard2.getPosition(0, 3).ship.name).toBe(ship1);
+    expect(testBoard2.getPosition(0, 3).position).toBe(3);
+    expect(testBoard2.getPosition(0, 4).ship.name).toBe(ship1);
+    expect(testBoard2.getPosition(0, 4).position).toBe(4);
 
-    testBoard.placeShip(4, 3, 5, "horizontal");
-    expect(testBoard.shipCount).toBe(2);
+    expect(testBoard2.getPosition(4, 3).ship.name).toBe(ship1);
+
+    console.log("gameboard - place ships - ship 2");
+    const name2 = `Ship-${shipCounter.incCounter()}`;
     const ship2 = `Ship-2`;
+    testBoard2.placeShip(name2, 4, 3, 5, "horizontal");
 
-    expect(testBoard.board[4][3].ship.name).toBe(ship2);
-    expect(testBoard.board[4][3].position).toBe(0);
-    expect(testBoard.board[4][4].ship.name).toBe(ship2);
-    expect(testBoard.board[4][4].position).toBe(1);
-    expect(testBoard.board[4][5].ship.name).toBe(ship2);
-    expect(testBoard.board[4][5].position).toBe(2);
-    expect(testBoard.board[4][6].ship.name).toBe(ship2);
-    expect(testBoard.board[4][6].position).toBe(3);
-    expect(testBoard.board[4][7].ship.name).toBe(ship2);
-    expect(testBoard.board[4][7].position).toBe(4);
+    expect(testBoard2.getPosition(4, 3).ship.name).toBe(ship2);
+    expect(testBoard2.getPosition(4, 3).position).toBe(0);
+    expect(testBoard2.getPosition(4, 4).ship.name).toBe(ship2);
+    expect(testBoard2.getPosition(4, 4).position).toBe(1);
+    expect(testBoard2.getPosition(4, 5).ship.name).toBe(ship2);
+    expect(testBoard2.getPosition(4, 5).position).toBe(2);
+    expect(testBoard2.getPosition(4, 6).ship.name).toBe(ship2);
+    expect(testBoard2.getPosition(4, 6).position).toBe(3);
+    expect(testBoard2.getPosition(4, 7).ship.name).toBe(ship2);
+    expect(testBoard2.getPosition(4, 7).position).toBe(4);
 
-    testBoard.placeShip(2, 2, 5, "vertical");
+    console.log("gameboard - place ships - ship 3");
+    const name3 = `Ship-${shipCounter.incCounter()}`;
     const ship3 = `Ship-3`;
+    testBoard2.placeShip(name3, 2, 2, 5, "vertical");
 
-    expect(testBoard.board[2][2].ship.name).toBe(ship3);
-    expect(testBoard.board[2][2].position).toBe(0); // 2
-    expect(testBoard.board[3][2].ship.name).toBe(ship3);
-    expect(testBoard.board[3][2].position).toBe(1); // 2
-    expect(testBoard.board[4][2].ship.name).toBe(ship3);
-    expect(testBoard.board[4][2].position).toBe(2); // 2
-    expect(testBoard.board[5][2].ship.name).toBe(ship3);
-    expect(testBoard.board[5][2].position).toBe(3); // 2
-    expect(testBoard.board[6][2].ship.name).toBe(ship3);
-    expect(testBoard.board[7][2].position).toBe(4); // 2
+    expect(testBoard2.getPosition(2, 2).ship.name).toBe(ship3);
+    expect(testBoard2.getPosition(2, 2).position).toBe(0); // 2
+    expect(testBoard2.getPosition(3, 2).ship.name).toBe(ship3);
+    expect(testBoard2.getPosition(3, 2).position).toBe(1); // 2
+    expect(testBoard2.getPosition(4, 2).ship.name).toBe(ship3);
+    expect(testBoard2.getPosition(4, 2).position).toBe(2); // 2
+    expect(testBoard2.getPosition(5, 2).ship.name).toBe(ship3);
+    expect(testBoard2.getPosition(5, 2).position).toBe(3); // 2
+    expect(testBoard2.getPosition(6, 2).ship.name).toBe(ship3);
+    expect(testBoard2.getPosition(7, 2).position).toBe(4); // 2
+    console.log("gameboard - place ships - exit");
 });
 
 test("gameboard - receive attack", () => {});
